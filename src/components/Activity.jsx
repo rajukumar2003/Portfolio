8a6a19e6ac9dc2c92ed085b323fcca5451c8e141
Raju kumar <rajukumarsingh12003@gmail.com>
2024-06-17 03:01:11 +0530

    Added vs code activity

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -3,13 +3,18 @@ import React, { useEffect, useState } from 'react';
 function Activity() {
     const [currentActivity, setCurrentActivity] = useState(null);
     const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+    const [activityStartTime, setActivityStartTime] = useState(null);
+
 
     useEffect(() => {
         const ws = new WebSocket('wss://portfolio-backend-otc0.onrender.com');
 
         ws.onmessage = (message) => {
             const activity = JSON.parse(message.data);
+            // console.log(activity);           //debugging
             setCurrentActivity(activity);
+            setActivityStartTime(Date.now() - activity.elapsed); // Initialize the activity start time
+
         };
 
         const clockInterval = setInterval(() => {
@@ -29,62 +34,93 @@ function Activity() {
         const { startTime, endTime } = currentActivity;
 
         const totalDuration = Math.max(1, Math.floor(new Date(endTime) - new Date(startTime)));
-        const currentTime = new Date(); 
-
+        const currentTime = new Date();
         // Calculate elapsed time based on the difference between current time and start time
         const elapsedTime = Math.max(0, Math.min(currentTime - new Date(startTime), totalDuration));
-
         return (elapsedTime / totalDuration) * 100; // Progress as a percentage
     };
+
+     const formatElapsedTime = (startTime) => {
+        const elapsedMilliseconds = Date.now() - startTime;
+        const elapsedSeconds = Math.floor(elapsedMilliseconds / 1000);
+        const hours = Math.floor(elapsedSeconds / 3600);
+        const minutes = Math.floor((elapsedSeconds % 3600) / 60);
+        const seconds = elapsedSeconds % 60;
+        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    };
+
+
 
 
     return (
         <div className="bg-gray-900 text-white py-6 ">
             <div className="container mx-auto text-center">
-                <hr className=" border-t w-4/5 border-gray-600 mx-auto mb-5" /> 
+                <hr className=" border-t w-4/5 border-gray-600 mx-auto mb-5" />
 
                 <p className=' text-left pl-8  pb-4 text-[#b3dcf2] text-2xl font-bold'>activity :</p>
 
                 {currentActivity ? (
-                    <div className="mb-4 flex flex-col items-center md:flex-row md:items-start md:space-x-4" key={currentActivity.id}>
-                        {currentActivity.image && (
-                            <div className="w-16 h-16 mb-4 md:mb-0 relative">
+                    currentActivity.activity === 'Spotify' ? (
+                        <div className="mb-4 flex flex-col items-center md:flex-row md:items-start md:space-x-4" key={currentActivity.id}>
+                            {currentActivity.image && (
+                                <div className="w-16 h-16 mb-4 md:mb-0 relative">
+                                    <a href={currentActivity.songUrl} target="_blank" rel="noopener noreferrer">
+                                        <img
+                                            src={currentActivity.image}
+                                            alt={currentActivity.activity}
+                                            className=" w-16 h-16coverobject- rounded-full animate-spin"
+                                        />
+                                    </a>
+                                </div>
+                            )}
+                            <div className="text-left w-60">
+                                <h3 className="text-lg font-bold text-blue-300  md:pb-0">@{currentActivity.username}</h3>
+                                <p className="text-orange-300">{currentActivity.status}</p>
+                                <div className="text-green-300 ">{activityStartTime ? formatElapsedTime(activityStartTime) : '00:00:00'} elapsed</div>
 
-                                <a href={currentActivity.songUrl} target="_blank" rel="noopener noreferrer">
+                                <p className=" text-lg text-gray-400 pb-2 pt-2">{currentActivity.activity}</p>
+
+                                <p className=" font-bold text-xl text-cyan-300 break-words">{currentActivity.songName}</p>
+                                <p className="text-gray-500 break-words">by {currentActivity.artistName}</p>
+                                {/* Song progress bar */}
+                                <div className=" w-3/4 bg-gray-700 rounded-full h-2 mt-4 mb-2">
+                                    <div
+                                        style={{ width: `${calculateProgress()}%` }}
+                                        className="bg-orange-300 rounded-full h-full transition-transform duration-500 ease-linear"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="mb-4 flex flex-col items-center md:flex-row md:items-start md:space-x-4" key={currentActivity.id}>
+                            {currentActivity.image && (
+                                <div className="w-16 h-16 mb-4 md:mb-0 relative">
                                     <img
                                         src={currentActivity.image}
                                         alt={currentActivity.activity}
-                                        className=" w-16 h-16coverobject- rounded-full animate-spin"
+                                        className="w-16 h-16 cover object-contain rounded"
                                     />
-                                </a>
-                            </div>
-                        )}
-                        <div className="text-left w-60">
-                            <h3 className="text-lg font-bold text-blue-300  md:pb-0">@{currentActivity.username}</h3>
-                            <p className="text-orange-300">{currentActivity.status}</p>
-                            <div className=" text-green-300 mb-4">{currentTime}</div> {/* Real-time clock */}
+                                </div>
+                            )}
+                            <div className="text-left w-60">
+                                <h3 className="text-lg font-bold text-blue-300  md:pb-0">@{currentActivity.username}</h3>
+                                <p className="text-orange-300">{currentActivity.status}</p>
+                                <div className="text-green-300 ">{activityStartTime ? formatElapsedTime(activityStartTime) : '00:00:00'} elapsed</div>
 
-                            <p className=" text-lg text-green-300 pb-2">{currentActivity.activity}</p>
-
-                            <p className=" font-bold text-xl text-gray-400 break-words">{currentActivity.songName}</p>
-                            <p className="text-gray-500 break-words">by {currentActivity.artistName}</p>
-                            {/* Song progress bar */}
-                            <div className=" w-3/4 bg-gray-800 rounded-full h-2 mt-4 mb-2">
-                                <div
-                                    style={{ width: `${calculateProgress()}%` }}
-                                    className="bg-orange-300 rounded-full h-full transition-transform duration-500 ease-linear"
-                                />
+                                <p className=" text-lg text-cyan-300 pb-2 pt-2">{currentActivity.activity}</p>
+                                <p className=" font-bold text-xl text-gray-400 break-words">{currentActivity.project}</p>
+                                <p className="text-gray-500 break-words">{currentActivity.fileName}</p>
+                                
                             </div>
                         </div>
+                    )
+                ) : (
+                    <div className="text-center pt-10">
+                        <p className="text-lg font-bold text-blue-300 pb-3 md:pb-0">@raju_kumar_12003</p>
+                        <p className="text-orange-300">Offline</p>
+                        <p className="text-green-300">{currentTime}</p>
                     </div>
-                ) :
-
-                    (
-                        <div className="text-center pt-10">
-                            <p className="text-lg font-bold text-blue-300 pb-3 md:pb-0">@raju_kumar_12003</p>
-                            <p className="text-orange-300">Offline</p>
-                            <p className="text-green-300">{currentTime}</p>
-                        </div>)}
+                )}
             </div>
         </div>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
-import { FaEnvelope, FaDiscord, FaGithub } from 'react-icons/fa';
+import { FaEnvelope, FaInstagram, FaGithub } from 'react-icons/fa';
 
 function Footer() {
     return (
         <div className=" bg-gray-900 text-white text-center pb-12">
             <hr className=" border-t w-3/5 border-gray-600 mx-auto mb-5" /> 
             <div className=" pt-6 flex flex-col md:flex-row justify-between items-center w-3/5 mx-auto space-y-4 md:space-y-0">
-                <div className=' flex space-x-4'>
-                    <FaEnvelope className="text-orange-300  hover:text-orange-200" />
-                    <FaDiscord className="  text-orange-300 text-xl hover:text-orange-200" />
-                    <FaGithub className="text-orange-300 text-xl hover:text-orange-200" />
+                <div className=' flex space-x-8 text-orange-300'>
+                    <a href="https://github.com/rajukumar2003/" target='_blank'><FaGithub className="text-orange-300 text-xl hover:text-orange-200" /></a>
+                    <a href="https://www.instagram.com/rajukumarsingh2003/" target='_blank'><FaInstagram className=" text-xl hover:text-orange-200" /></a>
+                    <a href="mailto:dev.rajukumarsingh@gmail.com"><FaEnvelope className="text-orange-300 text-xl hover:text-orange-200" /></a>
                 </div>
                 <div className=" md:font-bold text-purple-300">Made with <span className=" text-orange-300">{'<'}3,</span> Thank you! </div>
             </div>
